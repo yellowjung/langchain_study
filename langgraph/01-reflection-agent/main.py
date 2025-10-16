fc83d621e2d96a3f462cd907fe14a0d3e9649551
Youngdon Jung <youngdon.jung@lotte.net>
2025-10-16 15:21:18 +0900

    fix(langgraph): Update reflection agent implementation
    - Updates the implementation of the reflection agent in langgraph/01-reflection-agent/main.py.

--- langgraph/01-reflection-agent/main.py
+++ langgraph/01-reflection-agent/main.py
@@ -1,34 +1,40 @@
-from typing import List, Sequence
+from typing import List, Sequence, TypedDict, Annotated
 
 from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_core.messages import BaseMessage, HumanMessage
-from langgraph.graph import END, MessageGraph
-
+from langgraph.graph import END, StateGraph
+from langgraph.graph.message import add_messages
 from chains import generation_chain, reflect_chain
 
 REFLECT = "reflect"
 GENERATE = "generate"
 
-def generation_node(state: Sequence[BaseMessage]):
-    return generation_chain({"messages": state})
+class MessageGraph(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
 
-def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
-    res = reflect_chain.invoke({"messages": messages})
-    return [HumanMessage(content=res.content)]
 
-builder = MessageGraph()
+def generation_node(state: MessageGraph):
+    return {"messages": [generation_chain.invoke({"messages": state["messages"]})]}
+
+def reflection_node(state: MessageGraph):
+    res = reflect_chain.invoke({"messages": state["messages"]})
+    return {"messages": [HumanMessage(content=res.content)]}
+
+builder = StateGraph(state_schema=MessageGraph)
 builder.add_node(GENERATE, generation_node)
 builder.add_node(REFLECT, reflection_node)
 builder.set_entry_point(GENERATE)
 
-def should_continue(state: List[BaseMessage]):
-    if len(state) > 6:
+
+
+def should_continue(state: MessageGraph):
+    if len(state["messages"]) > 6:
         return END
     return REFLECT
 
-builder.add_conditional_edges(GENERATE, should_continue, {END: END, REFLECT: REFLECT})
+builder.add_conditional_edges(GENERATE, should_continue, path_map = {END:END, REFLECT:REFLECT})
 builder.add_edge(REFLECT, GENERATE)
 
 graph = builder.compile()
